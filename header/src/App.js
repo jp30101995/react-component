7cdc2070024f9717533fc7d3179d248cce87ed77
jp30101995 <jp30101995@gmail.com>
2018-07-12 14:14:01 +0530

    Checking for push

--- header/src/App.js
+++ header/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export default () =>
-  <header>
-    <h1>Logo</h1>
-    <nav>
-      <ul>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  </header>;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React2</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Questions />
+      </div>
+    );
+  }
+}
+
+export default App;
